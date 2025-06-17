268d4e30129444ffb4aa6177a38e26fb6305114c
Huszka Adrián Gábor <adrian.huszka@gmail.com>
2025-06-17 16:22:00 +0200

    update CORS configuration to allow multiple origins; change server port to 5300

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import tanugyi_adatok from "./controllers/tanugyi_adatok.controller.js";
 import cors from "cors";
 
 const corsConfig = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "http://172.16.0.100:5174",
+    "https://indicator.pollak.info",
+  ],
 };
 
 const app = express();
@@ -16,6 +20,6 @@ app.use(cors(corsConfig));
 app.use("/api/v1/alapadatok", alapadatokRouter);
 app.use("/api/v1/tanugyi_adatok", tanugyi_adatok);
 
-app.listen(3300, () => {
+app.listen(5300, () => {
   console.log("Elindult a http://localhost:3300");
 });
