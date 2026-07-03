06152e6450e16f8aaa27e771f586613ac763c1b6
Huszka Adrián <adrian.huszka@gmail.com>
2026-07-03 09:38:50 +0200

    33-as history

--- services/szervezetfejlesztes.service.js
+++ services/szervezetfejlesztes.service.js
@@ -1,4 +1,5 @@
 import prisma from "../utils/prisma.js";
+import { scheduleSnapshot } from "./form_history.service.js";
 
 /**
  * Get all records for a specific school and academic year
@@ -22,7 +23,7 @@ export const getSzervezetfejlesztes = async (alapadatokId, tanev) => {
  * @param {Object} data - The data to create
  */
 export const createSzervezetfejlesztes = async (data) => {
-  return await prisma.szervezetfejlesztes.create({
+  const created = await prisma.szervezetfejlesztes.create({
     data: {
       alapadatok_id: data.alapadatok_id,
       tanev_kezdete: parseInt(data.tanev_kezdete),
@@ -30,6 +31,8 @@ export const createSzervezetfejlesztes = async (data) => {
       resztvevok_szama: data.resztvevok_szama,
     },
   });
+  scheduleSnapshot(created.alapadatok_id, "szervezetfejlesztes");
+  return created;
 };
 
 /**
@@ -38,7 +41,7 @@ export const createSzervezetfejlesztes = async (data) => {
  * @param {Object} data - The data to update
  */
 export const updateSzervezetfejlesztes = async (id, data) => {
-  return await prisma.szervezetfejlesztes.update({
+  const updated = await prisma.szervezetfejlesztes.update({
     where: { id: parseInt(id) },
     data: {
       alapadatok_id: data.alapadatok_id,
@@ -47,6 +50,8 @@ export const updateSzervezetfejlesztes = async (id, data) => {
       resztvevok_szama: data.resztvevok_szama,
     },
   });
+  scheduleSnapshot(updated.alapadatok_id, "szervezetfejlesztes");
+  return updated;
 };
 
 /**
@@ -54,7 +59,9 @@ export const updateSzervezetfejlesztes = async (id, data) => {
  * @param {number} id - The ID of the record to delete
  */
 export const deleteSzervezetfejlesztes = async (id) => {
-  return await prisma.szervezetfejlesztes.delete({
+  const deleted = await prisma.szervezetfejlesztes.delete({
     where: { id: parseInt(id) },
   });
+  scheduleSnapshot(deleted.alapadatok_id, "szervezetfejlesztes");
+  return deleted;
 };
